75eb8c33c41d5559be08e32e9c6c10713632dfbd
Pickzhang <26913175742@qq.com>
2023-08-03 20:18:40 +0800

    third commit

---
+++ .cache/sync-requires.js
@@ -0,0 +1,13 @@
+const { hot } = require("react-hot-loader/root")
+
+// prefer default export if available
+const preferDefault = m => (m && m.default) || m
+
+
+exports.components = {
+  "component---cache-dev-404-page-js": hot(preferDefault(require("H:\\BaiduSyncdisk\\Projects\\Gatsby_Projects\\123\\gatsby-airtable-listing-master\\.cache\\dev-404-page.js"))),
+  "component---src-pages-404-jsx": hot(preferDefault(require("H:\\BaiduSyncdisk\\Projects\\Gatsby_Projects\\123\\gatsby-airtable-listing-master\\src\\pages\\404.jsx"))),
+  "component---src-pages-index-jsx": hot(preferDefault(require("H:\\BaiduSyncdisk\\Projects\\Gatsby_Projects\\123\\gatsby-airtable-listing-master\\src\\pages\\index.jsx"))),
+  "component---src-templates-single-item-jsx": hot(preferDefault(require("H:\\BaiduSyncdisk\\Projects\\Gatsby_Projects\\123\\gatsby-airtable-listing-master\\src\\templates\\single-item.jsx")))
+}
+
